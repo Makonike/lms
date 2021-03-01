ede0fa03ef8032968b0e5da4b327eff14c30ae78
Makonike <1694585033@qq.com>
2021-03-02 00:32:45 +0800

    update BaseServlet

--- src/com/makonike/web/BaseServlet.java
+++ src/com/makonike/web/BaseServlet.java
@@ -33,8 +33,8 @@ public abstract class BaseServlet extends HttpServlet {
         }
         Method method = null;
         try {
+            // this指继承自BaseServlet的、与所请求的url相匹配的Servlet
             method = this.getClass().getDeclaredMethod(methodName,HttpServletRequest.class,HttpServletResponse.class);
-            method.invoke(this,req,resp);
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException("你要调用的方法" + methodName +"不存在");
